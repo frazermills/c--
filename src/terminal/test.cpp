c79bf5e097046716bf34a76143e3834f4c9bdb13
Hayden Farrell <haydenfarrell@outlook.com>
2023-11-04 21:10:38 +0000

    can now navigate properly

--- src/terminal/test.cpp
+++ src/terminal/test.cpp
@@ -1,16 +1,23 @@
 #include <terminal.h>
+#include <array>
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <iterator>
+#include <string>
 #include <math.h>
 
 void init(){
     winmain = initscr();
-    line = 10;
+    line = 1;
     int row,col;
     getmaxyx(winmain, row, col);
     maxLines = row;
     insert = true;
     frame = 0;
     leftPad = 8;
-    xPos = leftPad;
+    xPos = 0;
+    yPos = 0;
     refresh();
 
     start_color();
@@ -33,38 +40,40 @@ void CleanUp(){
     endwin();
 }
 
-void SaveToFile(std::vector<int> text){
+void SaveToFile(std::array<std::array<int, 80>, 1000> text){
     std::ofstream File("code.txt", std::ios::out| std::ios::binary);
     
     int index = 0;
     int lines = 0;
-    for (char c : text){
-        // check if before or after line count
-        if(c == 10){
-            index = 0;
-            File << std::endl;
-            lines++;
+    for (auto charLine : text){
+        for (char c : charLine){
+            // check if before or after line count
+            if((index > 3 + ((lines+1) / 10)) && (c != '@') && (c != 0) && (c != '$'))
+                File << c;
+            index++;
         }
-        else if(index > 3 + ((lines+1) / 10) && c != 0)
-            File << c;
-        index++;
+        index = 0;
+        lines++;
     }
     File.close();
 }
 
-std::vector<int> PadLine(std::vector<int> text){
-    std::string s = std::to_string(line);
+std::array<std::array<int, 80>, 1000> PadLine(std::array<std::array<int, 80>, 1000> text){
+    std::string s = std::to_string(line * 10);
     for(char cha : s){
-        text.push_back(cha);
+        text[yPos][xPos++] = cha;
     }
-    text.push_back('\t');
-    text.push_back(0);
+    text[yPos][xPos] = '\t';
+    xPos += 4;
+    text[yPos][xPos++] = '$';
+    xPos++;
 
     return text;
 }
 
-std::vector<int> HandleInput(std::vector<int> text, int c){
+std::array<std::array<int, 80>, 1000> HandleInput(std::array<std::array<int, 80>, 1000> text, int c){
     // TODO: make not physicaly painful to look at
+    int lastX = xPos - 1;
     if(c < 10)
         return text;
 
@@ -72,51 +81,60 @@ std::vector<int> HandleInput(std::vector<int> text, int c){
         insert = !insert;
     }
     else if(insert){
-        if((c == 127) || (c == 263)){
-            if(text.size() > 3 + floor(line / 10)){
-                if(text.back() == 0){
-                    while (text.back() != 10)
-                    {
-                        text.pop_back();
-                        xPos--;
-                    }
-                    line -= 10;
-                    yPos--;
-                }
-                text.pop_back();
-                xPos--;
-            }
-        }
-        else if (c == 16)
+        switch (c)
         {
+            //Backspace
+        case 263:
+            if(text[yPos][--lastX] == '$'){
+                if(yPos != 0){
+                    for(int i = xPos; i >= 0; i--){
+                        text[yPos][i] = '@';
+                    }
+                    yPos--;
+
+                    //find last char pos of new line
+                    int lastIndex = 8;
+                    int currentIndex = 0;
+                    for(char character : text[yPos]){
+                        if(character != '@' && currentIndex > lastIndex){
+                            lastIndex = currentIndex;
+                            //printw("%d", character);
+                            //getch();
+                        }
+
+                        currentIndex++;
+                    }
+                    xPos = lastIndex;
+                }
+            }else{
+                text[yPos][--xPos] = '@';
+            }
+            
+            // more checks needed
+            break;
+        case 16:
             wclear(winmain);
             printw("Saving file... Enter to continue");
             SaveToFile(text);
             getch();
             wclear(winmain);
-        }else if(c == 10){
-            int row,col;
-            getmaxyx(winmain, row, col);
-
-            line += 10;
-            text.push_back(c);
+            break;
+        case 10:
+            line += 1;
+            text[yPos++][xPos] = '\n';
+            xPos = 0;
 
             text = PadLine(text);
-            yPos += 1;
-            xPos = 8;
-
-        }else if(c == '\t'){
+            break;
+        case '\t':
             for(int i = 0; i < 2; i++){
-                text.push_back(' ');
+                text[yPos][xPos++] = ' ';
             }
-            xPos+=2;
-        }
-        else{
-            if((c > 96) && (c < 123))
-                text.push_back(c);
-            else
-                text.push_back(c);
-            xPos++;
+            break;
+        
+        default:
+            text[yPos][xPos++] = c;
+            break;
         }
     }else{
         switch (c)
@@ -141,18 +159,23 @@ std::vector<int> HandleInput(std::vector<int> text, int c){
             xPos = leftPad;
         if(yPos <= 0)
             yPos = 0;
-        if(yPos >= (line / 10) - 1)
-            yPos = (line / 10) - 1;
+        if(yPos >= line - 1)
+            yPos = line - 1;
     }
 
     return text;
 }
 
-int main(int argc, char ** argv)
+int main(int argc, char **argv)
 {
     init();
     int currentChar;
-    std::vector<int> text;
+    std::array<std::array<int, 80>, 1000> text;
+
+    for(int i = 0; i < 1000; i++){
+        for(int j = 0 ; j < 80; j++)
+            text[i][j] = '@';
+    }
 
     text = PadLine(text);
 
@@ -164,37 +187,20 @@ int main(int argc, char ** argv)
         wclear(winmain);
 
         int newLines = 0;
-        int diff = (line - (maxLines * 10)) / 10;
+        int diff = line - maxLines;
         int linesPrinted = 0;
         int charCount = 0;
 
-        for(int c : text){
-            if(((maxLines * 10) < line) && (newLines < diff)){
-                if(c == 10)
-                    newLines++;
-            }else{
-
-                // screen jitter of beacause its a pain
-                /*
-                if((frame % 100 == 0) && linesPrinted == 0){
-                    if(c == 10 || charCount == 0){
-                        //wprintw(winmain, "\n");
-                    }
-                }
-                */
-                if(c == 10)
-                    linesPrinted++;
-
-                wprintw(winmain, "%c", c);
+        for(auto charLine : text){
+            for(int c : charLine){
+                if(c != '@' && c != '$')
+                    wprintw(winmain, "%c", c);
             }
-            charCount++;
         }
 
         wmove(winmain, yPos, xPos);
         wrefresh(winmain);
 
-        if(frame > 10000)
-            frame /= 100;
         currentChar = wgetch(winmain);
         text = HandleInput(text, currentChar);
     }
