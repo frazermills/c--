6c7b33dd40399c711355e7d4e68c6900adae4482
Hayden Farrell <haydenfarrell@outlook.com>
2023-11-04 15:00:14 +0000

    Moved input handling away from main

--- src/terminal/test.cpp
+++ src/terminal/test.cpp
@@ -20,6 +20,25 @@ void SaveToFile(std::vector<int> text){
     File.close();
 }
 
+std::vector<int> HandleInput(std::vector<int> text, int c){
+    if((c == 127) || (c == 263)){
+        if(text.size() > 0)
+            text.pop_back();
+    }
+    else if (c == 16)
+    {
+        wclear(winmain);
+        printw("Saving file... Enter to continue");
+        SaveToFile(text);
+        getch();
+        wclear(winmain);
+    }
+    else
+        text.push_back(c);
+
+    return text;
+}
+
 int main(int argc, char ** argv)
 {
     init();
@@ -30,21 +49,7 @@ int main(int argc, char ** argv)
         currentChar = wgetch(winmain);
         wclear(winmain);
 
-        if((currentChar == 127) || (currentChar == 263)){
-            if(text.size() > 0)
-                text.pop_back();
-        }
-        else if (currentChar == 16)
-        {
-            wclear(winmain);
-            printw("Saving file... Enter to continue");
-            SaveToFile(text);
-            getch();
-            wclear(winmain);
-        }
-        else
-            text.push_back(currentChar);
-
+        text = HandleInput(text, currentChar);
 
         for(int c : text){
             wprintw(winmain, "%c", c);
