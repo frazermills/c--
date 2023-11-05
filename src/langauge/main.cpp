dd00de28b663c3c87263171bc14785e34c0abf7a
frazer <frazer@t480>
2023-11-05 08:55:19 +0000

    got file input to work

--- src/langauge/main.cpp
+++ src/langauge/main.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 #include "lexer/token.h"
 #include "lexer/lexer.h"
 #include "parser/ast.h"
 
+std::string read_file(std::string file_name)
+{
+    std::string file_string, std_string_line;
+
+    std::fstream file(file_name);
+
+    while(std::getline(file, std_string_line))
+    {
+        for(auto c : std_string_line){
+            file_string.push_back(c);
+        }
+    }
+
+    return file_string;
+}
+
 int main(int argc, char **argv) {
-    std::string input = "fn test (int a, char c);";
+    std::string input = read_file(argv[1]);
     Token tok;
 
     do {
---
+++ src/langauge/parser/ast.h
@@ -0,0 +1,5 @@
+#ifndef AST_H_
+#define AST_H_
+
+#include "../lexer/token.h"
+#endif // AST_H_
